52fed4fd660fdb9e5902a6c5fa525b63ebb5453e
WindyYam <geniusyzy@qq.com>
2018-08-15 19:05:51 +0800

    myRover init

---
+++ mk.sh
@@ -0,0 +1,3 @@
+pushd APMrover2
+make apm2 BUILDROOT=../out
+popd 
---
+++ upload.bat
@@ -0,0 +1,1 @@
+D:\arduino-1.6.11\hardware\tools\avr/bin/avrdude -CD:\arduino-1.6.11\hardware\tools\avr/etc/avrdude.conf -v -patmega2560 -cwiring -PCOM24 -b115200 -D -Uflash:w:build/APMrover2.hex:i 
